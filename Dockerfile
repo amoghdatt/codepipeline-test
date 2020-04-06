41f5bfdae0d2e67681e17fc8b158615b99e0926d
amoghdatt <ubuntu@ip-172-31-84-1.ec2.internal>
2020-04-06 07:47:30 +0000

    added: ORM capability

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,19 @@ FROM python:3.6
 RUN apt-get update && \
     apt-get install -y && \
     apt-get install build-essential && \
+    curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add - && \
+    curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list > /etc/apt/sources.list.d/mssql-release.list && \
+    apt-get update -y && \
+    ACCEPT_EULA=Y apt-get install msodbcsql17 -y && \
+    ACCEPT_EULA=Y apt-get install mssql-tools -y && \
+    echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile && \
+    echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc && \
+    . ~/.bashrc && \
+    apt-get install unixodbc-dev -y && \
     pip install flask pandas cython && \
-    pip install pymssql && \
+    pip install pymssql flask_sqlalchemy pyodbc && \
     pip install pytest
+
 ADD run.py /usr/src/profile/
 ADD test.py /usr/src/profile/
 EXPOSE 5000
--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from flask import Flask
 import pymssql
 import pandas as pd
+from flask_sqlalchemy import SQLAlchemy
+import pyodbc
+import urllib
 
 server = '192.168.4.117'
 database = 'FreedomCashLenders'
@@ -8,21 +11,43 @@ username = 'FreedomCashLendersAll'
 mssql_password = 'Freedom123$'
 mysql_password = 'FreedomML123$'
 
-conn = pymssql.connect(server,username,mssql_password,database,port=1433)
+params_iloans = urllib.parse.quote_plus(
+    'DRIVER={ODBC Driver 17 for SQL Server};'
+    'Server='+server+';'+
+    'Database='+database+';'+
+    'UID='+username+';'+
+    'PWD='+mssql_password+';'+
+    'TDS_Version=8.0;'+
+    'Port=1433;')
 
-df_sample = pd.read_sql_query('SELECT TOP 1 LoanId FROM view_FCL_Loan',con=conn)
+#conn = pymssql.connect(server,username,mssql_password,database,port=1433)
+
+#df_sample = pd.read_sql_query('SELECT TOP 1 LoanId FROM view_FCL_Loan',con=conn)
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc:///?odbc_connect={}'.format(params_iloans)
+db = SQLAlchemy(app)
+
+class GetCreditDataLoan(db.Model):
+        __tablename__ = 'view_FCL_GetCreditDataLoan'
+        __table_args__ = {'extend_existing': True}
+
+        LoanId = db.Column(db.String,primary_key=True)
+        BankTransactionId = db.Column(db.String(100))
+
+gcd = GetCreditDataLoan.query.filter(
+    GetCreditDataLoan.BankTransactionId=='ca432336-5028-40d9-81dd-3b9b1b81c6b0'
+).first()
 
 
 @app.route('/')
 def test():
-    return {'message':df_sample.shape}
+    return {'message':str(gcd.LoanId)}
 
 @app.route('/test')
 def test2():
-    return {'message':'deployed to ebs using codepipeline'}
+    return {'message':gcd.BankTransactionId}
 
 
 if __name__ == "__main__":
